f2918e85d5f83dc81b6130627b31688cd722cee1
s-wada523 <potapota.bake@gmail.com>
2021-03-26 23:01:46 +0900

    タイトルの変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Twitter Cloneをはじめよう
+                    Twitter Clone
                 </div>
 
                 <!-- <div class="links">
